b831005719df18baf3bb0ad3f11fa0167e2b9f66
Roger <rogerpatriota@gmail.com>
2022-09-24 21:49:08 -0300

    Adiciona templates

--- routes/ucBp.py
+++ routes/ucBp.py
@@ -1,4 +1,4 @@
-from  flask import Blueprint
+from  flask import Blueprint, render_template
 from ..extensions import db
 from ..models.usuario import Usuario
 
@@ -6,5 +6,12 @@ ucBp = Blueprint('ucBp', __name__)
 
 @ucBp.route('/')
 def home():
-    db.create_all()
-    return 'Firts Page'
+    #db.create_all()
+    # filtra do banco pelo id quem vai vir
+    # pega o nome do usuario
+    # passa no html
+    return render_template('home/home_page.html')
+
+@ucBp.route('/teste')
+def teste():
+    return render_template('base_logged.html')
